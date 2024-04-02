00e2e03ce075ac2484e4867532eb9678766bb5bd
Jefferson BIEN-AIME <ancgate@yahoo.fr>
2024-04-02 19:32:58 -0400

    adding main.py for demo. Yulia get the latest version

---
+++ scripts/main.py
@@ -0,0 +1,11 @@
+import json
+
+# Specify the path to your JSON configuration file
+config_file_path = 'config/config.json'
+
+# Load the JSON configuration file
+with open(config_file_path, 'r') as config_file:
+    config = json.load(config_file)
+
+# Print the configuration
+Connection_STRING = config["connectionString"]
